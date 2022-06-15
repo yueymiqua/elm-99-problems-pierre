ff15d5b5b88197dcdb1f943692267be46b63d81d
Pierre Yue <pierre@shopbonsai.ca>
2022-06-15 12:14:29 -0400

    first commit

--- src/Lists.elm
+++ src/Lists.elm
@@ -1,27 +1,74 @@
 module Lists exposing (..)
 
+import Maybe exposing (Maybe(..))
+
 
 last : List a -> Maybe a
 last l =
-    -- Write a function that returns the last element of a list
-    Debug.todo "not implemented"
+    let
+        lastElement listInput =
+            listInput
+                |> List.reverse
+                |> List.head
+    in
+    lastElement l
 
 
-lastTwo : List a -> Maybe ( a, a )
+
+-- Write a function that returns the last element of a list
+
+
+lastTwo :
+    List a
+    -> ( Maybe a, Maybe a ) -- Have to fix to type Maybe (a, a)
 lastTwo l =
-    -- Find the last but one (last and penultimate) elements of a list.
-    Debug.todo "not implemented"
+    let
+        lastAndPen listInput =
+            listInput
+                |> List.reverse
+                |> List.take 2
+                |> List.reverse
+
+        lastElement =
+            List.head (lastAndPen l)
+
+        penElement =
+            List.head <| List.reverse (lastAndPen l)
+    in
+    ( lastElement, penElement )
+
+
+
+-- Find the last but one (last and penultimate) elements of a list.
 
 
 duplicate : List a -> List a
 duplicate l =
-    -- Duplicate the elements of a list.
-    -- e.g. [1,2,3] -> [1,1,2,2,3,3]
-    Debug.todo "not implemented"
+    let
+        emptyList =
+            []
+
+        newList inputList =
+            List.map (\x -> x :: x :: emptyList) inputList
+    in
+    List.concat (newList l)
+
+
+
+-- Duplicate the elements of a list.
+-- e.g. [1,2,3] -> [1,1,2,2,3,3]
+-- Debug.todo "not implemented"
 
 
 isPalindrome : List a -> Bool
 isPalindrome l =
-    -- Find out whether a list is a palindrome.
-    -- HINT: a palindrome is its own reverse.
-    Debug.todo "not implemented"
+    let
+        reverseList inputList =
+            List.reverse inputList
+    in
+    l == reverseList l
+
+
+
+-- Find out whether a list is a palindrome.
+-- HINT: a palindrome is its own reverse.
